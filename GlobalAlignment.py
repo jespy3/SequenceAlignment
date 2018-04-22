167c4f0296c1a19a8b85dae7048f78c90c0e61ec
Jahsh <joshua.espinas@gmail.com>
2018-04-22 17:08:01 +1200

    solved maths to construct the dictionary taking into account file was read. Implemented function to assign correct order to AAcombo

--- GlobalAlignment.py
+++ GlobalAlignment.py
@@ -9,25 +9,70 @@ f = open('BLOSUM62.txt', 'r')
 lineList = f.read().splitlines()    # list of each line of BLOSUM62 matrix
 
 AA = list('ARNDCQEGHILKMFPSTWYV')               # list of amino acids
-AAcombos =  list(combinations_with_replacement(AA, 2)) # initialise list for amino acid combos
+AAcombos = list(combinations_with_replacement(AA, 2)) # initialise list for amino acid combos
 BLOSUM = {}                                     # initialise dictionary to contain BLOSUM62 values
-#print(AAcombos)
-#print(len(AAcombos))
 
-# Accessing each permutation of two amino acids and appending to AAcombos
-# for i, ival in enumerate(AA):
-#     for j, jval in enumerate(AA):
-#         AAcombos.append(ival+jval)
 
-index = 0
+
+# Constructs the dictionary for BLOSUM62
 for i, line in enumerate(lineList):
-    #print(line)
-    # print(line.split('\t'))
+    AAindex = i     # Keeps track of index of AAcombos in how 'BLOSUM62.txt' is read. Acts as sum variable
     for j, num in enumerate(line.split('\t')):
-        key = AAcombos[index]
+        if j != 0:
+            AAindex += (len(lineList)-j)    # updating to correct AAcombos index
+        key = AAcombos[AAindex]
         BLOSUM[key] = int(num)
-        print(f"key={key} inserted into BLOSUM with value={int(num)}")
-        index += 1
-         
-    
-    print('Next row \n')
+
+# Establishing amino acid order
+AAorder = 'ARNDCQEGHILKMFPSTWYV'
+orderValue = {}                         # to assign each amino acid with an orderValue
+for i, AA in enumerate(AAorder):
+    orderValue[AAorder[i]] = i          # constructing orderValue dictionary
+
+def orderAAs(AAcombo):
+    '''
+    Re-arranges the order of the amino acid combination for searching in the BLOSUM62 matrix.
+
+    :param AAcombo: string of two letters according to amino acids, in any order
+    :return: string rearranged in the amino acid order 'ARNDCQEGHILKMFPSTWYV'
+    '''
+
+    firstAA = AAcombo[0]
+    secondAA = AAcombo[1]
+    if orderValue[firstAA] > orderValue[secondAA]:
+        AAcombo = AAcombo[::-1]               # Reverses AAcombo order if not in order
+    return AAcombo
+
+class AminoAcid:
+
+
+    def __init__(self, AAcombo):
+        self.AAcombo = AAcombo
+
+        AAorder = 'ARNDCQEGHILKMFPSTWYV'
+        self.orderValue = {}  # to assign each amino acid with an orderValue
+        for i, AA in enumerate(AAorder):
+            self.orderValue[AAorder[i]] = i
+
+    def OrderAAs(self):
+        '''
+        Re-arranges the order of the amino acid combination for searching in the BLOSUM62 matrix.
+
+        :param AAcombo: string of two letters according to amino acids, in any order
+        :return: string rearranged in the amino acid order 'ARNDCQEGHILKMFPSTWYV'
+        '''
+
+        firstAA = self.AAcombo[0]
+        secondAA = self.AAcombo[1]
+        if self.orderValue[firstAA] > self.orderValue[secondAA]:
+            self.AAcombo = self.AAcombo[::-1]
+        return self.AAcombo
+
+
+
+test = 'CD'
+test = orderAAs(test)
+tuple = (test[0],test[1])
+
+x = BLOSUM[tuple]
+print(x)
