aa7ab87574543f063dbdf16eff112cd9a2834794
Jahsh <joshua.espinas@gmail.com>
2018-05-01 16:47:00 +1200

    Made Viterbi calculations easier with extra log function

--- ViterbiAlgorithm.py
+++ ViterbiAlgorithm.py
@@ -3,14 +3,22 @@
 import math
 import numpy as np
 
-base = 2
-z = 0 + math.log(1, base)
+def log2(prob):
+    if prob == 0:
+        return -float('inf')
+    return math.log(prob,2)
 
-a = -4.644 + -float('inf')
-b = -0.644 + math.log(0.2, base)
-c = -2.644 + math.log(0.6, base)
-pres = [a, b, c]
+
+z = 0 + log2(0)
+inf = -float('inf')
+
+a = inf + log2(0.8)
+b = inf + log2(0.8)
+c = inf + log2(0)
+
+pres = [a, b, c, z]
 
 print(pres)
 x = np.amax(pres)
 print(x)
+
